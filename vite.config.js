854ed8bb977c0a9f6252fe89e868d2ef0b26b38e
VoxWRX <149512265+VoxWRX@users.noreply.github.com>
2025-11-27 10:34:56 +0100

    remove manual chunks to fix blank page

--- vite.config.js
+++ vite.config.js
@@ -4,28 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // On garde juste l'augmentation de la limite pour ne pas avoir le warning jaune
   build: {
-    // On augmente la limite d'alerte à 1000 ko (1 mo) pour être tranquille
-    chunkSizeWarningLimit: 1000,
-
-    rollupOptions: {
-      output: {
-        // C'est ici qu'on configure le découpage manuel
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            // On isole SEULEMENT Recharts (c'est lui le plus lourd)
-            if (id.includes("recharts")) {
-              return "recharts";
-            }
-
-            // --- MODIFICATION ICI ---
-            // On a supprimé le bloc qui isolait 'react' et 'react-dom'.
-            // On met tout le reste (y compris React) dans un fichier "vendor" unique.
-            // C'est beaucoup plus sûr pour éviter les bugs de chargement.
-            return "vendor";
-          }
-        },
-      },
-    },
+    chunkSizeWarningLimit: 1600,
   },
 });
